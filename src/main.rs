8e4ff740b7cafcc362446acc8c7c91484a8d6cb0
Yaron Tausky <ytausky@protonmail.com>
2019-12-16 02:24:52 +0100

    Create struct RunningCpu

--- src/main.rs
+++ src/main.rs
@@ -103,77 +103,90 @@ impl Regs {
 
 impl Cpu {
     pub fn step(&mut self, input: CpuInput) -> Option<BusOp> {
-        self.exec_instr(input)
+        RunningCpu {
+            regs: &mut self.regs,
+            ctrl: &mut self.ctrl,
+            input: &input,
+        }
+        .exec_instr()
     }
+}
 
+struct RunningCpu<'a> {
+    regs: &'a mut Regs,
+    ctrl: &'a mut RunningState,
+    input: &'a CpuInput,
+}
+
+impl<'a> RunningCpu<'a> {
     #[inline(always)]
-    fn exec_instr(&mut self, input: CpuInput) -> Option<BusOp> {
+    fn exec_instr(&mut self) -> Option<BusOp> {
         match self.ctrl.opcode.split() {
-            (0b00, 0b000, 0b000) => self.nop(input),
-            (0b01, 0b110, 0b110) => self.halt(input),
-            (0b01, 0b110, src) => self.ld_deref_hl_r(src.into(), input),
-            (0b01, dest, 0b110) => self.ld_r_deref_hl(dest.into(), input),
-            (0b01, dest, src) => self.ld_r_r(dest.into(), src.into(), input),
-            (0b11, 0b001, 0b001) => self.ret(input),
+            (0b00, 0b000, 0b000) => self.nop(),
+            (0b01, 0b110, 0b110) => self.halt(),
+            (0b01, 0b110, src) => self.ld_deref_hl_r(src.into()),
+            (0b01, dest, 0b110) => self.ld_r_deref_hl(dest.into()),
+            (0b01, dest, src) => self.ld_r_r(dest.into(), src.into()),
+            (0b11, 0b001, 0b001) => self.ret(),
             _ => unimplemented!(),
         }
     }
 
-    fn nop(&mut self, input: CpuInput) -> Option<BusOp> {
-        self.fetch(input)
+    fn nop(&mut self) -> Option<BusOp> {
+        self.fetch()
     }
 
-    fn ld_r_r(&mut self, dest: R, src: R, input: CpuInput) -> Option<BusOp> {
+    fn ld_r_r(&mut self, dest: R, src: R) -> Option<BusOp> {
         match (self.ctrl.m_cycle, self.ctrl.phase) {
-            (M1, Tick) => self.fetch(input),
+            (M1, Tick) => self.fetch(),
             (M1, Tock) => {
                 let value = *self.regs.reg(src);
                 *self.regs.reg(dest) = value;
-                self.fetch(input)
+                self.fetch()
             }
             _ => unreachable!(),
         }
     }
 
-    fn ld_r_deref_hl(&mut self, dest: R, input: CpuInput) -> Option<BusOp> {
+    fn ld_r_deref_hl(&mut self, dest: R) -> Option<BusOp> {
         match (self.ctrl.m_cycle, self.ctrl.phase) {
             (M1, Tick) => {
                 self.advance();
                 Some(BusOp::Read(self.regs.hl()))
             }
             (M1, Tock) => {
-                *self.regs.reg(dest) = input.data.unwrap();
+                *self.regs.reg(dest) = self.input.data.unwrap();
                 self.advance()
             }
-            (M2, _) => self.fetch(input),
+            (M2, _) => self.fetch(),
             _ => unreachable!(),
         }
     }
 
-    fn ld_deref_hl_r(&mut self, src: R, input: CpuInput) -> Option<BusOp> {
+    fn ld_deref_hl_r(&mut self, src: R) -> Option<BusOp> {
         match (self.ctrl.m_cycle, self.ctrl.phase) {
             (M1, Tick) => {
                 self.advance();
                 Some(BusOp::Write(self.regs.hl(), *self.regs.reg(src)))
             }
             (M1, Tock) => self.advance(),
-            (M2, _) => self.fetch(input),
+            (M2, _) => self.fetch(),
             _ => unreachable!(),
         }
     }
 
-    fn halt(&mut self, _input: CpuInput) -> Option<BusOp> {
+    fn halt(&mut self) -> Option<BusOp> {
         unimplemented!()
     }
 
-    fn ret(&mut self, input: CpuInput) -> Option<BusOp> {
+    fn ret(&mut self) -> Option<BusOp> {
         match (self.ctrl.m_cycle, self.ctrl.phase) {
             (M1, Tick) => {
                 self.advance();
                 Some(BusOp::Read(self.regs.sp))
             }
             (M1, Tock) => {
-                self.regs.pc = input.data.unwrap().into();
+                self.regs.pc = self.input.data.unwrap().into();
                 self.regs.sp += 1;
                 self.advance()
             }
@@ -182,12 +195,12 @@ impl Cpu {
                 Some(BusOp::Read(self.regs.sp))
             }
             (M2, Tock) => {
-                self.regs.pc |= u16::from(input.data.unwrap()) << 8;
+                self.regs.pc |= u16::from(self.input.data.unwrap()) << 8;
                 self.regs.sp += 1;
                 self.advance()
             }
             (M3, _) => self.advance(),
-            (M4, _) => self.fetch(input),
+            (M4, _) => self.fetch(),
         }
     }
 
@@ -202,14 +215,14 @@ impl Cpu {
         None
     }
 
-    fn fetch(&mut self, input: CpuInput) -> Option<BusOp> {
+    fn fetch(&mut self) -> Option<BusOp> {
         match self.ctrl.phase {
             Phase::Tick => {
                 self.ctrl.phase = Tock;
                 Some(BusOp::Read(self.regs.pc))
             }
             Phase::Tock => {
-                self.ctrl.opcode = Opcode(input.data.unwrap());
+                self.ctrl.opcode = Opcode(self.input.data.unwrap());
                 self.regs.pc += 1;
                 self.ctrl.m_cycle = MCycle::M1;
                 None
