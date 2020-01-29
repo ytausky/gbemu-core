fdc96f1729aa60036ded26cf124e14dfcf41d9f6
Yaron Tausky <ytausky@protonmail.com>
2020-01-29 23:43:23 +0100

    Test JP cc, nn

--- src/cpu/tests/branch.rs
+++ src/cpu/tests/branch.rs
@@ -34,3 +34,66 @@ fn jp_nz_nn_non_branching() {
     cpu.regs.f.z = true;
     cpu.test_simple_instr(&[0xc2, 0x34, 0x12], &[])
 }
+
+#[test]
+fn jp_z_nn_branching() {
+    let mut cpu = Cpu::default();
+    cpu.regs.f.z = true;
+    cpu.test_opcode(
+        &[0xca, 0x34, 0x12],
+        &[
+            (Input::with_data(None), None),
+            (Input::with_data(None), None),
+            (Input::with_data(None), Some(BusOp::Read(0x1234))),
+            (Input::with_data(Some(0x00)), None),
+        ],
+    )
+}
+
+#[test]
+fn jp_z_nn_non_branching() {
+    let mut cpu = Cpu::default();
+    cpu.test_simple_instr(&[0xca, 0x34, 0x12], &[])
+}
+
+#[test]
+fn jp_nc_nn_branching() {
+    let mut cpu = Cpu::default();
+    cpu.test_opcode(
+        &[0xd2, 0x34, 0x12],
+        &[
+            (Input::with_data(None), None),
+            (Input::with_data(None), None),
+            (Input::with_data(None), Some(BusOp::Read(0x1234))),
+            (Input::with_data(Some(0x00)), None),
+        ],
+    )
+}
+
+#[test]
+fn jp_nc_nn_non_branching() {
+    let mut cpu = Cpu::default();
+    cpu.regs.f.cy = true;
+    cpu.test_simple_instr(&[0xd2, 0x34, 0x12], &[])
+}
+
+#[test]
+fn jp_c_nn_branching() {
+    let mut cpu = Cpu::default();
+    cpu.regs.f.cy = true;
+    cpu.test_opcode(
+        &[0xda, 0x34, 0x12],
+        &[
+            (Input::with_data(None), None),
+            (Input::with_data(None), None),
+            (Input::with_data(None), Some(BusOp::Read(0x1234))),
+            (Input::with_data(Some(0x00)), None),
+        ],
+    )
+}
+
+#[test]
+fn jp_c_nn_non_branching() {
+    let mut cpu = Cpu::default();
+    cpu.test_simple_instr(&[0xda, 0x34, 0x12], &[])
+}
