066ceeb9c4302472ffe30dad287b5e5f89b2a241
Yaron Tausky <ytausky@protonmail.com>
2020-01-29 20:36:52 +0100

    Write tests for JP NZ, nn

--- src/cpu/tests/branch.rs
+++ src/cpu/tests/branch.rs
@@ -13,3 +13,26 @@ fn jp_nn() {
         ],
     )
 }
+
+#[ignore]
+#[test]
+fn jp_nz_nn_branching() {
+    let mut cpu = Cpu::default();
+    cpu.test_opcode(
+        &[0xc2, 0x34, 0x12],
+        &[
+            (Input::with_data(None), None),
+            (Input::with_data(None), None),
+            (Input::with_data(None), Some(BusOp::Read(0x1234))),
+            (Input::with_data(Some(0x00)), None),
+        ],
+    )
+}
+
+#[ignore]
+#[test]
+fn jp_nz_nn_non_branching() {
+    let mut cpu = Cpu::default();
+    cpu.regs.f.z = true;
+    cpu.test_simple_instr(&[0xc2, 0x34, 0x12], &[])
+}
